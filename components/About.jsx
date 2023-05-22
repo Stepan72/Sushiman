6c403636a45366d99dfb254a27dd1f0dd2829e5f
Stepan72 <st.nim@list.ru>
2023-05-22 11:53:08 +0600

    about middle ver 0.22

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import "../styles/sections/about.scss";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <section className="about-us" id="about-us">
+      <div className="about-us__image">
+        <div className="about-us__image-sushi3">
+          <img src="/assets/sushi-3.png" alt="sushi3-image" />
+        </div>
+        <button className="about-us__button">
+          Learn More
+          <img src="/assets/arrow-up-right.svg" alt="learn more" />
+        </button>
+        <div className="about-us__image-sushi2">
+          <img src="/assets/sushi-2.png" alt="sushi2-image" />
+        </div>
+      </div>
+      <div className="about-us__content">
+        <p className="sushi__subtitle">About Us / 私たちに関しては</p>
+        <h3 className="sushi__title">
+          Our mission is to bring true Japanese flavours to you.
+        </h3>
+        <p className="sushi__description">
+          We will continue to provide the experience of Omotenashi, the Japanese
+          mindset of hospitality, with our shopping and dining for our
+          customers.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
