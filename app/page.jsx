485453c804aae80daffa1f8ac9b0959325ad01de
Stepan72 <st.nim@list.ru>
2023-05-22 09:36:29 +0600

    about started ver 0.21

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default async function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   );
 }
