951e8d474bcb0f753daa9c582666dbba3010e0e7
Stepan72 <st.nim@list.ru>
2023-05-22 18:33:40 +0600

    menu completed ver 0.4

--- components/Card.jsx
+++ components/Card.jsx
@@ -16,12 +16,12 @@ function Card({ cardData }) {
           alt={`sushi-${cardData.cardNumber}`}
         />
         <h4 className="popular-food__card-title">{cardData.cardTitle}</h4>
-        <div className="popular-food__card-details flex-between">
+        <div className="popular-food__card-details flex-between ">
           <div className="popular-food__card-rating">
             <img src="/assets/star.svg" alt="star" />
             <p>{`${cardData.cardRating.toFixed(1)}`}</p>
           </div>
-          <p className="popular-food__card-price">{`$${cardData.cardPrice.toFixed(
+          <p className="popular-food__card-price ">{`$${cardData.cardPrice.toFixed(
             2
           )}`}</p>
         </div>
