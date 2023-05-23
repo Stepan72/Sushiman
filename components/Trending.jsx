d29c943d5105584dba60597a6d1a157804468ae8
Stepan72 <st.nim@list.ru>
2023-05-23 20:21:56 +0600

    trending finish ver 0.5

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -5,11 +5,19 @@ import { slideIn } from "@/utils/motion";
 import "../styles/sections/trending.scss";
 const sushiData = [
   { title: "Make Sushi" },
-  { title: "Uramaki Sushi  " },
+  { title: "Uramaki Sushi" },
   { title: "Temaki Sushi" },
   { title: "Oshizushi" },
   { title: "Nigiri Sushi" },
   { title: "Inari Sushi" },
+];
+const sushiDrinks = [
+  { title: "Oruncha" },
+  { title: "Aojiru" },
+  { title: "Kombu-cha" },
+  { title: "Sakura Tea" },
+  { title: "Ofukucha" },
+  { title: "Mugicha" },
 ];
 
 function Trending() {
@@ -57,11 +65,52 @@ function Trending() {
             </div>
           </div>
         </section>
-      </section>
 
-      <div className="trending__discover">
-        <p>Discover</p>
-      </div>
+        <div className="trending__discover">
+          <p>Discover</p>
+        </div>
+        <section className="trending-drink">
+          <div className="trending__image flex-center">
+            <motion.img
+              src="/assets/sushi-4.png"
+              alt="sushi-4"
+              variants={slideIn("left", "tween", 0.1, 0.4)}
+              initial="hidden"
+              whileInView="show"
+            />
+            <div className="trending__arrow trending__arrow-right">
+              <img src="/assets/arrow-vertical.svg" alt="arrow vertical" />
+            </div>
+            <div className="trending__arrow trending__arrow-top">
+              <img src="/assets/arrow-horizontal.svg" alt="arrow horizontal" />
+            </div>
+          </div>
+          <motion.div
+            className="trending__content"
+            variants={slideIn("right", "tween", 0.1, 0.4)}
+            initial="hidden"
+            whileInView="show"
+          >
+            <p className="sushi__subtitle">What’s Trending / トレンド</p>
+            <h3 className="sushi__title">Japanese Drinks</h3>
+            <p className="sushi__description">
+              Feel the taste of the most delicious Japense drinks here.
+            </p>
+            <ul className="trending__list flex-between">
+              {sushiDrinks.map((el, index) => {
+                return (
+                  <li key={index}>
+                    <div className="trending__icon flex-center">
+                      <img src="/assets/check.svg" alt="check" />
+                    </div>
+                    <p>{el.title}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        </section>
+      </section>
     </>
   );
 }
