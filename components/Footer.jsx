8692283699467d993a05a9c74f6405fffd15163b
Stepan72 <st.nim@list.ru>
2023-05-24 09:56:47 +0600

    subscribe finish ver 0.55

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/sections/navbar.scss";
+import { motion } from "framer-motion";
+import { navVariants } from "@/utils/motion";
+
+function Footer() {
+  return <div>Footer</div>;
+}
+
+export default Footer;
--- components/Subscription.jsx
+++ components/Subscription.jsx
@@ -7,14 +7,20 @@ import "../styles/sections/subscribe.scss";
 function Subscription() {
   return (
     <section className="subscription flex-center" id="services">
-      <h2>
-        Get offers stright <br /> to your inbox
-      </h2>
-      <p>Sign up for the Sushiman newsletter</p>
-      <div className="subscription__form">
-        <input type="text" placeholder="Enter you email address" />
-        <button type="button">Get started</button>
-      </div>
+      <motion.div
+        variants={slideIn("up", "tween", 0.1, 0.5)}
+        initial="hidden"
+        whileInView="show"
+      >
+        <h2>
+          Get offers stright <br /> to your inbox
+        </h2>
+        <p>Sign up for the Sushiman newsletter</p>
+        <div className="subscription__form">
+          <input type="text" placeholder="Enter you email address" />
+          <button type="button">Get started</button>
+        </div>
+      </motion.div>
     </section>
   );
 }
