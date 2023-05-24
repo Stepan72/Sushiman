529b1e3b25d7816b1560119c541694736122d0a8
Stepan72 <st.nim@list.ru>
2023-05-24 09:50:30 +0600

    subscribe middle ver 0.52

--- components/Subscription.jsx
+++ components/Subscription.jsx
@@ -5,7 +5,18 @@ import { slideIn } from "@/utils/motion";
 import "../styles/sections/subscribe.scss";
 
 function Subscription() {
-  return <div>Subscription</div>;
+  return (
+    <section className="subscription flex-center" id="services">
+      <h2>
+        Get offers stright <br /> to your inbox
+      </h2>
+      <p>Sign up for the Sushiman newsletter</p>
+      <div className="subscription__form">
+        <input type="text" placeholder="Enter you email address" />
+        <button type="button">Get started</button>
+      </div>
+    </section>
+  );
 }
 
 export default Subscription;
